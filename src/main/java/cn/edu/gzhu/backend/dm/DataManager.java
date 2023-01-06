5266abf6b1e247caa5436038d399d51c661035d6
LinJiayu <531467600@qq.com>
2023-01-06 16:51:51 +0800

    引用计数缓存框架和共享内存数组

--- src/main/java/cn/edu/gzhu/backend/dm/DataManager.java
+++ src/main/java/cn/edu/gzhu/backend/dm/DataManager.java
@@ -6,6 +6,15 @@ import cn.edu.gzhu.backend.dm.page.PageCache;
 import cn.edu.gzhu.backend.dm.page.PageOne;
 import cn.edu.gzhu.backend.tm.TransactionManager;
 
+/**
+ * DM的主要职责有：
+ *  1）分页管理 DB 文件，并进行缓存。
+ *  2）管理日志文件，保证在发生错误时可以根据日志进行回复。
+ *  3）抽象 DB 文件为 DataItem 供上层模块使用，并提供缓存。
+ * 可以归纳为以下两点：1. 上层模块和文件系统之间的抽象层，向下直接读取文件，向上提供数据的包装。 2. 日志功能
+ *
+ * 引入引用计数策略：只有上层模块主动释放引用，缓存在确保没有模块在使用这个资源了，才回去驱逐资源。
+ */
 public interface DataManager {
     DataItem read(long uid) throws Exception;
     long insert(long xid, byte[] data) throws Exception;
