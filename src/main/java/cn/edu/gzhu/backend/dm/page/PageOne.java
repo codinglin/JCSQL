d7fc84e7ef123da1ad9933ad4b643462ef2d19b5
LinJiayu <531467600@qq.com>
2023-01-06 16:52:50 +0800

    特殊管理页

--- src/main/java/cn/edu/gzhu/backend/dm/page/PageOne.java
+++ src/main/java/cn/edu/gzhu/backend/dm/page/PageOne.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * 特殊管理第一页
  * ValidCheck
  * db启动时给100~107字节处填入一个随机字节，db关闭时将其拷贝到108~115字节
- * 用于判断上一次数据库是否正常关闭
+ * 用于判断上一次数据库是否正常关闭，如果是异常关闭，就需要执行数据的恢复流程。
  */
 public class PageOne {
     private static final int OF_VC = 100;
